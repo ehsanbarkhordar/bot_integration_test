5e9e0053582c6ef804f27d121554fdd92c6886f7
baleadmin <dr.enyacc@gmail.com>
2019-01-19 15:11:17 +0330

    initial commit

---
+++ server.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+# WS server example
+import json
+
+import asyncio
+import websockets
+
+from balebot.models.base_models import FatSeqUpdate
+from balebot.models.messages import *
+from balebot.models.server_updates.message_update_body import MessageUpdate
+
+
+async def start_bot(websocket, path):
+    update_message_dict = {
+        "$type": "FatSeqUpdate",
+        "seq": 19900,
+        "body": {
+            "$type": "Message",
+            "peer": {
+                "$type": "User",
+                "id": 201707397,
+                "accessHash": "-2675024364153751067"
+            },
+            "sender": {
+                "$type": "User",
+                "id": 201707397,
+                "accessHash": "-2675024364153751067"
+            },
+            "date": "1547895898678",
+            "randomId": "-1872862833315134147",
+            "message": {
+                "$type": "Text",
+                "text": "/start"
+            }
+        },
+        "users": [],
+        "groups": []
+    }
+    update_message_str = json.dumps(update_message_dict)
+    await websocket.send(update_message_str)
+    # print(update_message_str)
+    #
+    name = await websocket.recv()
+    print(f"< {name}")
+    read_message_update_json = {"$type": "Response", "id": "0", "body": {"date": "1547895898824"}}
+    read_message_update_str = json.dumps(read_message_update_json)
+    await websocket.send(read_message_update_str)
+
+    websocket.close()
+
+    #
+    # greeting = f"Hello {name}!"
+    #
+    # await websocket.send(greeting)
+    # print(f"> {greeting}")
+
+
+# async def hello(websocket, path):
+#     await websocket.send("")
+#
+#     name = await websocket.recv()
+#     print(f"< {name}")
+#
+#     greeting = f"Hello {name}!"
+#
+#     await websocket.send(greeting)
+#     print(f"> {greeting}")
+
+
+start_server = websockets.serve(start_bot, 'localhost', '8765')
+print(start_server)
+print(type(start_server))
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
---
+++ test_bot.py
@@ -0,0 +1,50 @@
+import asyncio
+
+from balebot.models.messages import TemplateMessageButton, TextMessage, TemplateMessage
+from balebot.updater import Updater
+from balebot.utils.logger import Logger
+
+updater = Updater(token="token", loop=asyncio.get_event_loop())
+dispatcher = updater.dispatcher
+my_logger = Logger.get_logger()
+
+
+# =================================== Call Backs =======================================================
+def success_send_message(response, user_data):
+    kwargs = user_data['kwargs']
+    update = kwargs["update"]
+    user_peer = update.get_effective_user()
+    my_logger.info("success", extra={"user_id": user_peer.peer_id, "tag": "info"})
+
+
+def failure_send_message(response, user_data):
+    kwargs = user_data['kwargs']
+    bot = kwargs["bot"]
+    message = kwargs["message"]
+    update = kwargs["update"]
+    try_times = int(kwargs["try_times"])
+    if try_times < 5:
+        try_times += 1
+        user_peer = update.get_effective_user()
+        my_logger.error("failure", extra={"user_id": user_peer.peer_id, "tag": "error"})
+        kwargs = {"message": message, "update": update, "bot": bot, "try_times": try_times}
+        bot.respond(update=update, message=message, success_callback=success_send_message,
+                    failure_callback=failure_send_message, kwargs=kwargs)
+    else:
+        my_logger.error("max tried", extra={"tag": "error"})
+
+
+@dispatcher.command_handler("/start")
+def main_menu(bot, update):
+    user_peer = update.get_effective_user()
+    btn_list = [TemplateMessageButton(text="a", value="a", action=0),
+                TemplateMessageButton(text="b", value="b", action=0),
+                TemplateMessageButton(text="c", value="c", action=0)]
+    general_message = TextMessage("hahahahahah")
+    template_message = TemplateMessage(general_message=general_message, btn_list=btn_list)
+    second_kwargs = {"message": template_message, "update": update, "bot": bot, "try_times": 1}
+    bot.send_message(template_message, user_peer, success_callback=success_send_message,
+                     failure_callback=failure_send_message, kwargs=second_kwargs)
+
+
+updater.run()
